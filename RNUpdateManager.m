72bdce66d08aad484133d66cdd2e15d1f262e10b
lhl <271072547@coolpad.com>
2018-04-12 16:21:36 +0800

    必要的注释

--- RNUpdateManager.m
+++ RNUpdateManager.m
@@ -13,11 +13,40 @@
 #import "FileHash.h"
 
 @interface RNUpdateManager()
+
+/**
+ 更新bundle目录
+ */
 @property (nonatomic,copy) NSString* updateDir;
+
+/**
+ bundle备份目录
+ */
+@property (nonatomic,copy) NSString* updateBakeDir;
+
+/**
+ 下载的临时文件夹
+ */
 @property (nonatomic,copy) NSString* tempZipPath;
+
+/**
+ 解压文件的临时文件夹
+ */
 @property (nonatomic,copy) NSString* tempUnzipPath;
+
+/**
+ 持有查询到的更新信息
+ */
 @property (nonatomic,copy) NSDictionary* updateResult;
+
+/**
+ 存在本地plist文件的版本信息
+ */
 @property (nonatomic,strong) NSDictionary* versionInfo;
+
+/**
+ RCTBridge对象
+ */
 @property (nonatomic,strong) RCTBridge* bridge;
 
 @end
@@ -33,15 +62,24 @@
   return sharedInstance;
 }
 
+/**
+ 判断是否存在下载过的有效的bundle文件
+ */
 + (BOOL)isValidJsBundleExist{
   NSString* target = self.sharedManager.versionInfo[@"target"];
   return [target isEqualToString:CommonUtils.appVersion];
 }
 
+/**
+ 本地下载的bundle文件路径
+ */
 + (NSString*)updatedJsBundlePath{
   return [self.sharedManager.updateDir stringByAppendingPathComponent:@"main.jsbundle"];
 }
 
+/**
+ 更新文件夹路径
+ */
 - (NSString*)updateDir{
   if (!_updateDir) {
     _updateDir = [self dirWithPathName:@"update"];
@@ -49,6 +87,22 @@
   return _updateDir;
 }
 
+/**
+ 备份文件夹路径
+ */
+- (NSString*)updateBakeDir{
+  if (!_updateBakeDir) {
+    _updateBakeDir = [self dirWithPathName:@"update_bake"];
+  }
+  return _updateBakeDir;
+}
+
+/**
+ 在document文件目录下创建文件夹并返回路径
+
+ @param name 文件路径名称
+ @return 创建的文件夹路径
+ */
 - (NSString*)dirWithPathName:(NSString*)name{
   NSString *documentDir = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask,YES)[0];
   NSString *dir = [documentDir stringByAppendingPathComponent:name];
@@ -59,6 +113,9 @@
   return dir;
 }
 
+/**
+ 存储临时下载文件的目录
+ */
 - (NSString *)tempZipPath {
   if (!_tempZipPath) {
     _tempZipPath = [NSString stringWithFormat:@"%@/\%@.zip",
@@ -68,6 +125,9 @@
   return _tempZipPath;
 }
 
+/**
+ 临时接呀目录
+ */
 - (NSString *)tempUnzipPath {
   if (!_tempUnzipPath) {
     NSString *path = [NSString stringWithFormat:@"%@/\%@",
@@ -88,10 +148,17 @@
   return _tempUnzipPath;
 }
 
+
+/**
+ 本地plist存储的路径
+ */
 - (NSString*)versionInfoPath {
   return [self.updateDir stringByAppendingPathComponent:@"version.plist"];
 }
 
+/**
+ 获取plist存储的版本信息
+ */
 - (NSDictionary*)versionInfo{
   if (!_versionInfo) {
     NSString *plistPath = self.versionInfoPath;
@@ -104,6 +171,12 @@
   return _versionInfo;
 }
 
+
+/**
+ 检查更新文件
+
+ @param bridge RCTBridge对象
+ */
 + (void)checkWithBridge:(RCTBridge*)bridge {
   RNUpdateManager.sharedManager.bridge = bridge;
   dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
@@ -119,13 +192,17 @@
     if (serverVersion<=localVersion) {
       return;
     }
-    NSString* updateUrl = result[@"updateUrl"];
+    NSString* updateUrl = result[@"url"];
     [manager downloadWithUrl:updateUrl];
   });
 }
 
+/**
+ 下载更新文件
+
+ @param url 服务器保存的下载链接
+ */
 - (void)downloadWithUrl:(NSString*)url {
-  //根据url下载相关文件
   NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
   AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
   NSURL *URL = [NSURL URLWithString:url];
@@ -134,6 +211,7 @@
     //获取下载进度
     NSLog(@"Progress is %f", downloadProgress.fractionCompleted);
   } destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
+    //自定义下载路径
     return [NSURL fileURLWithPath:self.tempZipPath];
   } completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
     if(error){
@@ -145,6 +223,12 @@
   [downloadTask resume];
 }
 
+
+/**
+ 处理下载的文zip件
+
+ @param path 下载路径
+ */
 - (void)settleFileWithPath:(NSString*)path {
   NSString* hashCode = [FileHash md5HashOfFileAtPath:path];
   if (![hashCode isEqualToString:self.updateResult[@"hash"]]) {
